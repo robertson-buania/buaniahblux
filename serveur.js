549b6337ba59cbd938623825e6645b22e1f13605
RAISSA KAZZ <kazz>
2021-12-17 21:25:14 +0100

    parametrages

--- serveur.js
+++ serveur.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + "dist/hblux"))
@@ -8,3 +8,20 @@ app.get("/*", function(req, es) {
 
 
 app.listen(process.env.PORT);
+ */
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+const app = express();
+app.use(requireHTTPS);
+
+app.get("/*", function(req, res) {
+    res.sendFile("index.html", { root: "dist/hblux/" });
+});
